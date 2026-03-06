baea94fb03553f133878a78cc66750d7dfd5c0d6
thoriqmacto <thariq.hadad@gmail.com>
2026-03-06 13:35:03 +0300

    add route for debug local disk

--- apps/api/routes/web.php
+++ apps/api/routes/web.php
@@ -9,3 +9,12 @@ Route::get('/ping', function () {
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-storage', function () {
+    return [
+        'default_disk' => config('filesystems.default'),
+        'public_exists' => Storage::disk('public')->exists('template-fields/images/sJaHS4AhpG1RvZibLTIJ3Ue0BXGvSopKmJ7f39SH.png'),
+        'local_exists' => Storage::disk('local')->exists('template-fields/images/sJaHS4AhpG1RvZibLTIJ3Ue0BXGvSopKmJ7f39SH.png'),
+        'public_url' => Storage::disk('public')->url('template-fields/images/sJaHS4AhpG1RvZibLTIJ3Ue0BXGvSopKmJ7f39SH.png'),
+    ];
+});
