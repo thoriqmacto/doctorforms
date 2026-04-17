c26fa4c0788f3966eef6772d17c0d280a6e8189c
Thariq <thoriqgrady@yahoo.com>
2026-04-17 13:21:44 +0300

    Replace template field seeder with extracted sqlite dataset

--- apps/api/database/seeders/HospitalsTableSeeder.php
+++ apps/api/database/seeders/HospitalsTableSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace database\seeders;
+namespace Database\Seeders;
 
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
@@ -13,28 +13,29 @@ class HospitalsTableSeeder extends Seeder
      */
     public function run(): void
     {
-
-
-        DB::table('hospitals')->insert([
-            'id' => 1,
-            'name' => 'RSUD dr. Soedarso',
-            'address' => 'Jl. DR. Soedarso No.1, Bangka Belitung Laut, Kec. Pontianak Tenggara, Kota Pontianak, Kalimantan Barat 78124, Indonesia',
-            'phone' => '+62-561-737701',
-            'email' => 'tu.rsdrsoedarso@gmail.com',
-            'logo_url' => '/storage/logos/hospitals/rsud_soedarso.png',
-            'created_at' => Carbon::create(2023, 1, 1, 16, 14, 6),
-            'updated_at' => Carbon::create(2023, 1, 1, 16, 14, 6),
-        ]);
-
-        DB::table('hospitals')->insert([
-            'id' => 2,
-            'name' => 'RS Medika Djaya',
-            'address' => 'Jl. Parit H. Husin 1 Blok. MD No. 1, Bangka Belitung Laut, Kec. Pontianak Tenggara, Kota Pontianak, Kalimantan Barat 78124, Indonesia',
-            'phone' => '+62-561-5688558',
-            'email' => 'rsmedikadjaya@gmail.com',
-            'logo_url' => '/storage/logos/hospitals/rs_medika_djaya.png',
-            'created_at' => Carbon::create(2023, 1, 2, 16, 14, 6),
-            'updated_at' => Carbon::create(2023, 1, 2, 16, 14, 6),
-        ]);
+        DB::table('hospitals')->upsert([
+            [
+                'id' => 1,
+                'name' => 'RSUD dr. Soedarso',
+                'address' => 'Jl. DR. Soedarso No.1, Bangka Belitung Laut, Kec. Pontianak Tenggara, Kota Pontianak, Kalimantan Barat 78124, Indonesia',
+                'phone' => '+62-561-737701',
+                'email' => 'tu.rsdrsoedarso@gmail.com',
+                'logo_url' => '/storage/logos/hospitals/rsud_soedarso.png',
+                'logo_path' => 'logos/hospitals/rsud_soedarso.png',
+                'created_at' => Carbon::create(2023, 1, 1, 16, 14, 6),
+                'updated_at' => Carbon::create(2023, 1, 1, 16, 14, 6),
+            ],
+            [
+                'id' => 2,
+                'name' => 'RS Medika Djaya',
+                'address' => 'Jl. Parit H. Husin 1 Blok. MD No. 1, Bangka Belitung Laut, Kec. Pontianak Tenggara, Kota Pontianak, Kalimantan Barat 78124, Indonesia',
+                'phone' => '+62-561-5688558',
+                'email' => 'rsmedikadjaya@gmail.com',
+                'logo_url' => '/storage/logos/hospitals/rs_medika_djaya.png',
+                'logo_path' => 'logos/hospitals/rs_medika_djaya.png',
+                'created_at' => Carbon::create(2023, 1, 2, 16, 14, 6),
+                'updated_at' => Carbon::create(2023, 1, 2, 16, 14, 6),
+            ],
+        ], ['id']);
     }
 }
